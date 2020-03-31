ab4ea1d2029bf063ee5312c8c3a76bdb5b2cd7c7
arthur-ryoo <jene0523@gmail.com>
2020-03-30 23:16:08 -0700

    Add create, read, update, and delete funtionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Dog
 # Create your views here.
+from .forms import FeedingForm
 
 
 def home(request):
@@ -19,3 +22,35 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    feeding_form = FeedingForm()
+    return render(request, 'dogs/detail.html', {
+        'dog': dog, 'feeding_form': feeding_form
+    })
+
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
