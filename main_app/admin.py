0d4a85d5080164a6580df4ca733a6e41b0067400
arthur-ryoo <jene0523@gmail.com>
2020-03-31 23:22:45 -0700

    Add toy model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Dog, Feeding
-# Register your models here.
+from .models import Dog, Feeding, Toy
 
 admin.site.register(Dog)
 admin.site.register(Feeding)
+admin.site.register(Toy)
