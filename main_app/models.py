b78ec4ff5afcb0bd328f8fc1704edcf999958f87
arthur-ryoo <jene0523@gmail.com>
2020-03-31 23:26:22 -0700

    Create toy model

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 from datetime import date
-# Create your models here.
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -11,11 +10,20 @@ MEALS = (
 )
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"A {self.color} {self.name}"
+
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
